52866a5bd8dd6e4364be79bd90c4fc2dff266dee
Nick Hammond <nick@nickhammond.com>
2012-06-11 20:48:24 -0500

    make sure all relevant dirs are present before loading modules

--- recipes/source.rb
+++ recipes/source.rb
@@ -69,6 +69,51 @@ node.run_state[:nginx_force_recompile] = false
 node.run_state[:nginx_configure_flags] = 
   node[:nginx][:source][:default_configure_flags] | node[:nginx][:configure_flags]
 
+directory node[:nginx][:dir] do
+  owner "root"
+  group "root"
+  mode "0755"
+end
+
+directory node[:nginx][:log_dir] do
+  mode 0755
+  owner node[:nginx][:user]
+  action :create
+end
+
+%w(sites-available sites-enabled conf.d).each do |leaf|
+  directory File.join(node[:nginx][:dir], leaf) do
+    owner "root"
+    group "root"
+    mode "0755"
+  end
+end
+
+%w(nxensite nxdissite).each do |nxscript|
+  template "/usr/sbin/#{nxscript}" do
+    source "#{nxscript}.erb"
+    mode "0755"
+    owner "root"
+    group "root"
+  end
+end
+
+template "nginx.conf" do
+  path "#{node[:nginx][:dir]}/nginx.conf"
+  source "nginx.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :reload, 'service[nginx]', :immediately
+end
+
+template "#{node[:nginx][:dir]}/sites-available/default" do
+  source "default-site.erb"
+  owner "root"
+  group "root"
+  mode 0644
+end
+
 node[:nginx][:source][:modules].each do |ngx_module|
   include_recipe "nginx::#{ngx_module}"
 end
@@ -94,18 +139,6 @@ end
 
 node.run_state.delete(:nginx_configure_flags)
 node.run_state.delete(:nginx_force_recompile)
-
-directory node[:nginx][:dir] do
-  owner "root"
-  group "root"
-  mode "0755"
-end
-
-directory node[:nginx][:log_dir] do
-  mode 0755
-  owner node[:nginx][:user]
-  action :create
-end
 
 case node[:nginx][:init_style]
 when "runit"
@@ -171,8 +204,6 @@ else
     action :enable
   end
 end
-
-include_recipe 'nginx::commons'
 
 %w{nxensite nxdissite}.each do |nxscript|
   template "/usr/sbin/#{nxscript}" do
